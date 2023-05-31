1123548c9ccaf8021eb254cbb9331056ae0ea603
Joey Vanhalst <joey.vanhalst@skyline.be>
2023-05-31 10:14:35 +0200

    initial solution

---
+++ BPA Info_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BPA Info_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("BPA Info_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F9CCB5CE-8513-41A2-B168-A2C449E4C815")]
+[assembly: AssemblyVersion("1.0.0.0")]
