1006c421cfc1ada5304d54c9d9c2f9aecf0186db
Joey Vanhalst <joey.vanhalst@skyline.be>
2023-06-01 15:27:27 +0200

    added readme grouped by agent

--- BPA Info_1/BPA Info_1.cs
+++ BPA Info_1/BPA Info_1.cs
@@ -51,16 +51,14 @@ dd/mm/2023	1.0.0.1		XXX, Skyline	Initial version
 
 namespace BPA_Info_1
 {
-	using System;
 	using System.Collections.Generic;
-	using System.Globalization;
+	using System.ComponentModel;
 	using System.Linq;
-	using System.Security.Claims;
-	using System.Text;
 	using System.Text.RegularExpressions;
 	using AdaptiveCards;
 	using Newtonsoft.Json;
 	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 	using Skyline.DataMiner.Net.BPA;
 	using Skyline.DataMiner.Net.Messages;
 	using Skyline.DataMiner.Net.Messages.SLDataGateway;
@@ -76,225 +74,82 @@ namespace BPA_Info_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			var bpaResults = GetBpaResults();
-			//SendBpaResultsAsTable(engine, bpaResults);
-			//SendBpaResultsAsFactSets(engine, bpaResults);
-			SendBpaResultsAsContainer(engine, bpaResults);
+			var bpaResults = GetBpaResultsByAgent(engine);
+
+			SendBpaResults(engine, bpaResults);
 		}
 
-		private Dictionary<string, ExecuteBpaResponse> GetBpaResults()
+		private Dictionary<string, Dictionary<string, ExecuteBpaResponse>> GetBpaResultsByAgent(IEngine engine)
 		{
 			var helper = new BpaManagerHelper(Engine.SLNetRaw);
 			var bpas = helper.BPAs.ReadAll();
 
-			var executeBpasResponse = helper.BPAs.GetLastResults(helper.BPAs.ReadAll(), new DestinationAgent[] { new DestinationAgent(-1) });
-			return executeBpasResponse.Responses.ToDictionary(r => bpas.FirstOrDefault(b => b.ID == r.BpaId)?.MetaData?.TestName);
-		}
+			var dms = engine.GetDms();
+			var agents = dms.GetAgents();
 
-		private void SendBpaResultsAsTable(IEngine engine, Dictionary<string, ExecuteBpaResponse> bpaResults)
-		{
-			var table = new AdaptiveTable
+			var bpaResultsByAgent = new Dictionary<string, Dictionary<string, ExecuteBpaResponse>>();
+
+			var executeBpasResponse = helper.BPAs.GetLastResults(helper.BPAs.ReadAll(), agents.Select(a => new DestinationAgent(a.Id)).ToArray());
+			foreach (var response in executeBpasResponse.Responses)
 			{
-				Type = "Table",
-				FirstRowAsHeaders = true,
-				Columns = new List<AdaptiveTableColumnDefinition>
+				var dmaId = response.SourceDmaId;
+				var dmaName = agents.FirstOrDefault(a => a.Id == dmaId)?.Name;
+				if (dmaName == null)
 				{
-					new AdaptiveTableColumnDefinition
-					{
-						Width = 250,
-					},
-					new AdaptiveTableColumnDefinition
-					{
-						Width = 150,
-					},
-					new AdaptiveTableColumnDefinition
-					{
-						Width = 300,
-					},
-					new AdaptiveTableColumnDefinition
-					{
-						Width = 200,
-					},
-				},
-				Rows = new List<AdaptiveTableRow>
-				{
-					new AdaptiveTableRow
-					{
-						Type = "TableRow",
-						Cells = new List<AdaptiveTableCell>
-						{
-							new AdaptiveTableCell
-							{
-								Type = "TableCell",
-								Items = new List<AdaptiveElement>
-								{
-									new AdaptiveTextBlock("Name")
-									{
-										Type = "TextBlock",
-										Weight = AdaptiveTextWeight.Bolder,
-									},
-								},
-							},
-							new AdaptiveTableCell
-							{
-								Type = "TableCell",
-								Items = new List<AdaptiveElement>
-								{
-									new AdaptiveTextBlock("Status")
-									{
-										Type = "TextBlock",
-										Weight = AdaptiveTextWeight.Bolder,
-									},
-								},
-							},
-							new AdaptiveTableCell
-							{
-								Type = "TableCell",
-								Items = new List<AdaptiveElement>
-								{
-									new AdaptiveTextBlock("Message")
-									{
-										Type = "TextBlock",
-										Weight = AdaptiveTextWeight.Bolder,
-									},
-								},
-							},
-							new AdaptiveTableCell
-							{
-								Type = "TableCell",
-								Items = new List<AdaptiveElement>
-								{
-									new AdaptiveTextBlock("Last Run")
-									{
-										Type = "TextBlock",
-										Weight = AdaptiveTextWeight.Bolder,
-									},
-								},
-							},
-						},
-					},
-				},
-			};
+					continue;
+				}
 
-			foreach (var result in bpaResults)
-			{
-				var row = new AdaptiveTableRow
+				if (!bpaResultsByAgent.TryGetValue(dmaName, out var bpaResultsForAgent))
 				{
-					Type = "TableRow",
-					Cells = new List<AdaptiveTableCell>
-					{
-						new AdaptiveTableCell
-						{
-							Type = "TableCell",
-							Items = new List<AdaptiveElement>
-							{
-								new AdaptiveTextBlock(result.Key)
-								{
-									Type = "TextBlock",
-									Wrap = true,
-								},
-							},
-						},
-						new AdaptiveTableCell
-						{
-							Type = "TableCell",
-							Items = new List<AdaptiveElement>
-							{
-								new AdaptiveTextBlock(Regex.Replace(result.Value.Outcome.ToString(), @"([a-z])([A-Z])", "$1 $2"))
-								{
-									Type = "TextBlock",
-									Wrap = true,
-								},
-							},
-							Style = TranslateOutcome(result.Value.Outcome),
-						},
-						new AdaptiveTableCell
-						{
-							Type = "TableCell",
-							Items = new List<AdaptiveElement>
-							{
-								new AdaptiveTextBlock(result.Value.Message)
-								{
-									Type = "TextBlock",
-									Wrap = true,
-								},
-							},
-							Style = TranslateOutcome(result.Value.Outcome),
-						},
-						new AdaptiveTableCell
-						{
-							Type = "TableCell",
-							Items = new List<AdaptiveElement>
-							{
-								new AdaptiveTextBlock(result.Value.Timestamp.ToString())
-								{
-									Type = "TextBlock",
-									Wrap = true,
-								},
-							},
-						},
-					},
-				};
+					bpaResultsForAgent = new Dictionary<string, ExecuteBpaResponse>();
+					bpaResultsByAgent[dmaName] = bpaResultsForAgent;
+				}
 
-				table.Rows.Add(row);
+				var bpaName = bpas.FirstOrDefault(b => b.ID == response.BpaId)?.MetaData?.TestName;
+				bpaResultsForAgent[bpaName] = response;
 			}
 
-			var adaptiveCardBody = new List<AdaptiveElement> { table };
-
-			engine.AddScriptOutput("AdaptiveCard", JsonConvert.SerializeObject(adaptiveCardBody));
+			return bpaResultsByAgent;
 		}
 
-		private void SendBpaResultsAsFactSets(IEngine engine, Dictionary<string, ExecuteBpaResponse> bpaResults)
-		{
-			var adaptiveCardBody = new List<AdaptiveElement>()
-			{
-				new AdaptiveTextBlock("This is the overview of the results of the last run.")
-				{
-					Wrap = true,
-				},
-			};
-
-			foreach (var result in bpaResults)
-			{
-				adaptiveCardBody.Add(new AdaptiveFactSet()
-				{
-					Facts = new List<AdaptiveFact>
-					{
-						new AdaptiveFact("Name:", result.Key),
-						new AdaptiveFact("Status:", Regex.Replace(result.Value.Outcome.ToString(), @"([a-z])([A-Z])", "$1 $2")),
-						new AdaptiveFact("Message:", result.Value.Message),
-					},
-				});
-			}
-
-			engine.AddScriptOutput("AdaptiveCard", JsonConvert.SerializeObject(adaptiveCardBody));
-		}
-
-		private void SendBpaResultsAsContainer(IEngine engine, Dictionary<string, ExecuteBpaResponse> bpaResults)
+		private void SendBpaResults(IEngine engine, Dictionary<string, Dictionary<string, ExecuteBpaResponse>> bpaResultsbyAgent)
 		{
 			var adaptiveCardBody = new List<AdaptiveElement>();
 
-			foreach (var result in bpaResults.OrderBy(r => r.Key))
+			foreach (var bpaResultsForAgent in bpaResultsbyAgent.OrderBy(r => r.Key))
 			{
-				var factSet = new AdaptiveFactSet
+				var agentInfoTextBlock = new AdaptiveTextBlock($"BPA results for {bpaResultsForAgent.Key}")
 				{
-					Type = "FactSet",
-					Facts = new List<AdaptiveFact>
-					{
-						new AdaptiveFact("Name:", result.Key),
-						new AdaptiveFact("Status:", Regex.Replace(result.Value.Outcome.ToString(), @"([a-z])([A-Z])", "$1 $2")),
-						new AdaptiveFact("Message:", result.Value.Message),
-					},
+					Type = "TextBlock",
+					Weight = AdaptiveTextWeight.Bolder,
+					Size = AdaptiveTextSize.Large,
 				};
+				adaptiveCardBody.Add(agentInfoTextBlock);
 
-				var container = new AdaptiveContainer();
-				container.Items.Add(factSet);
-				container.Style = TranslateOutcome(result.Value.Outcome);
+				foreach (var result in bpaResultsForAgent.Value.OrderBy(r => r.Key))
+				{
+					var bpaInfoFacts = new AdaptiveFactSet
+					{
+						Type = "FactSet",
+						Facts = new List<AdaptiveFact>
+						{
+							new AdaptiveFact("Name", result.Key),
+							new AdaptiveFact("Status", Regex.Replace(result.Value.Outcome.ToString(), @"([a-z])([A-Z])", "$1 $2")),
+							new AdaptiveFact("Message:", result.Value.Message),
+							new AdaptiveFact("Executed:", result.Value.Timestamp.ToString()),
+						},
+					};
 
-				adaptiveCardBody.Add(container);
+					var bpaResultsContainer = new AdaptiveContainer();
+					bpaResultsContainer.Style = TranslateOutcome(result.Value.Outcome);
+					bpaResultsContainer.Items.Add(bpaInfoFacts);
+
+					adaptiveCardBody.Add(bpaResultsContainer);
+				}
 			}
 
-			engine.AddScriptOutput("AdaptiveCard", JsonConvert.SerializeObject(adaptiveCardBody));
+			var adaptiveCard = JsonConvert.SerializeObject(adaptiveCardBody);
+			engine.AddScriptOutput("AdaptiveCard", adaptiveCard);
 		}
 
 		private AdaptiveContainerStyle TranslateOutcome(BpaOutcome outcome)
@@ -308,7 +163,7 @@ namespace BPA_Info_1
 				case BpaOutcome.NoIssues:
 					return AdaptiveContainerStyle.Good;
 				default:
-					return AdaptiveContainerStyle.Default;
+					return AdaptiveContainerStyle.Emphasis;
 			}
 		}
 	}
